6b1170a61cf5d27c636dbc7c22aabac77dbb6018
TomKwanten <tomkwanten2001@gmail.com>
2025-09-01 11:23:59 +0200

    winkelmand altijd zichtbaar

--- productController.php
+++ productController.php
@@ -3,12 +3,28 @@
 declare(strict_types=1);
 
 require_once("init.php");
-require_once("initTwig.php"); // Twig initialiseren
+require_once("initTwig.php");
+require_once("initSession.php"); // om session te starten
 
 use Business\ProductService;
 
 $productSvc = new ProductService();
 $productenLijst = $productSvc->getAllProducts();
 
-print $twig->render("productlijst.twig", ["productenLijst" => $productenLijst]);
+// Winkelmandje-items samenstellen (zelfde structuur als in winkelmandController)
+$winkelmandje = [];
+foreach ($_SESSION['winkelmandje'] ?? [] as $id => $aantal) {
+    $product = $productSvc->getProductById((int)$id);
+    if ($product) {
+        $winkelmandje[] = ['product' => $product, 'aantal' => $aantal];
+    }
+}
 
+// Als je ingelogd bent
+$ingelogd = $_SESSION['klant'] ?? null;
+
+print $twig->render("productlijst.twig", [
+    "productenLijst" => $productenLijst,
+    "winkelmandje" => $winkelmandje,
+    "ingelogd" => $ingelogd
+]);
